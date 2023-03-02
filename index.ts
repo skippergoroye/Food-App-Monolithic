ebc31b775518da06d221d9c386b1c949216ec24b
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-03-02 17:28:13 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+
+const app = express();
+
+
+
+
+
+app.use('/', (req, res) => {
+    return res.json('Hello from Food order Backend')
+})
+
+
+
+
+app.listen(8000, () => {
+    console.log('App is listening on the port 8000')
+})
